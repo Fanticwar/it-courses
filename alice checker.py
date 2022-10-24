f95322a3822ae2f844a732567a4e8e3160943c64
Fanticwar <78257179+Fanticwar@users.noreply.github.com>
2022-10-24 20:57:29 +0100

    basic files
    idk

---
+++ alice checker.py
@@ -0,0 +1,9 @@
+print("What ur name")
+name = input()
+
+if name == "Alice":
+    print("Hi Alice")
+
+else:
+    print("Yr not Alice, shove off")
+    
---
+++ helloname.py
@@ -0,0 +1,17 @@
+#this program is like chicken:
+
+#chicken does input (seed)
+print("elo, what is your personal identification string? ")
+name = input()
+
+#chicken does output (egg)
+print("elo "+name+".")
+print("your name length is "+str(len(name))+".")
+
+#chicken gets to do input again (more seed)
+print("how many years have you been alive?")
+age = input()
+
+#chicken gets to do output again (more egg)
+print("in 1 year you will be "+str(int(age)+1)+" years old")
+
